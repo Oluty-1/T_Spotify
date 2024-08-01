56ae9ff39a94f557a2efff1520c46f9f64d9639b
Oluty-1 <tayoefunshile@gmail.com>
2024-08-01 02:11:48 +0100

    Playlist file

---
+++ .terraform/providers/registry.terraform.io/conradludgate/spotify/0.2.7/linux_amd64/playlist.tf
@@ -0,0 +1,24 @@
+resource "spotify_playlist" "playlist" {
+  name        = "Big 3"
+
+  tracks = [
+    data.spotify_search_track.name.tracks[0].id
+    data.spotify_search_track.name.tracks[1].id
+    data.spotify_search_track.name.tracks[2].id
+  ]
+}
+
+data "spotify_search_track" "cole" {
+  artist = "J.Cole"
+  limit = 10
+}
+
+data "spotify_search_track" "drizzy" {
+  artist = "Drake"
+  limit = 10
+}
+
+data "spotify_search_track" "dot" {
+  artist = "Kendrick"
+  limit = 10
+}
--- providers.tf
+++ providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     spotify = {
-      source = "louishuyng/spotify"
-      version = "0.3.6"
+      source = "conradludgate/spotify"
+      version = "0.2.7"
     }
   }
 }
